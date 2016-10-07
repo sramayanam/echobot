bc842a6c70c19c268b676e267f6cc78ae075d9ed
sramayanam <sreekanth_bhr@yahoo.com>
2016-10-07 12:00:25 -0500

    Updated code to version3

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get secrets from server environment
-var botConnectorOptions = { 
+/*var botConnectorOptions = { 
     appId: process.env.BOTFRAMEWORK_APPID, 
     appSecret: process.env.BOTFRAMEWORK_APPSECRET
  
-};
+};*/
 
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+/*
 // Create bot
 var bot = new builder.BotConnectorBot(botConnectorOptions);
 bot.add('/', function (session) {
@@ -18,8 +32,13 @@ bot.add('/', function (session) {
 
 // Setup Restify Server
 var server = restify.createServer();
+*/
 
 // Handle Bot Framework messages
+
+//create bot
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // Serve a static web page
@@ -28,6 +47,14 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
+
+bot.dialog('/', function (session) {
+    //session.send("Hello Sree" + session.message.text);
+
+    session.send("Hello Sree");
+});
+/*
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+*/
